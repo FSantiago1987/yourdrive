f2c0f89922654e8539eab445e9792a1aedad2e30
FSantiago1987 <rawpower_br@hotmail.com>
2021-07-06 11:06:09 -0700

    first commit

---
+++ src/app/components/logo/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import CarLogoImg from "../../../assets/images/car-logo.png";
+import tw from 'twin.macro';
+
+const LogoContainer = styled.div`
+    ${tw`
+        flex
+        item
+    `}
+`;
+
+function Logo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Logo
---
+++ src/app/pages/HomePage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const PageContainer = styled.div`
+    ${tw`
+        flex
+        flex-col
+        w-full
+        h-full
+        items-center
+        overflow-x-hidden
+    `}
+`;
+
+function HomePage() {
+    return (
+        <PageContainer>
+            Vamos Flamengo
+        </PageContainer>
+    )
+}
+
+export default HomePage;
